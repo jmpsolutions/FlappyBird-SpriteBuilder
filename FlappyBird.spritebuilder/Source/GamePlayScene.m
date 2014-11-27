78b27f65d156c56e6bac6bbb9053e00db791c527
Jair Manuel Poveda Frago <jair@htmlnatural.com>
2014-11-26 21:25:18 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -18,4 +18,31 @@
 
 // put new methods here
 
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
+
+
+
+
+
+
 @end
+//To declare a method in Objective-C, follow the following format:
+    // -(type I return)nameOfMethod: (type of first parameter) firstParameter continuationOfMethodName: (type of second parameter) secondParameter
+
+//Mas ejemplos de metodos
+    // -(void) doSomething
+    //{
+    //    NSLog(@"Hello World!");
+    //}
+
+//Mas ejemplos de metodos
+//-(int) doSomethingWithAString: (NSString*) myString
+//{
+    //returns the length of the string times ten
+//    return [myString length] * 10;
+//}
+
+
